5ac3f071ac8f07eb55c3fba765629dddaa1edb37
Obba Paschal <paschal.obba@deimos.co.za>
2021-02-17 02:12:03 +0100

    chore: remove keys

--- send-email.ts
+++ send-email.ts
@@ -1,6 +1,6 @@
 import * as mailgun from "mailgun-js";
-const WEBHOOK_SIGNING_KEY = '1cf0991b3ed95f29d785890fea066cde-4de08e90-032e944e'
-const mg = mailgun({apiKey: '1cf0991b3ed95f29d785890fea066cde-4de08e90-032e944e', domain: 'sandboxc0662581b5ed48a3a877b759c8c0f9d5.mailgun.org'});
+const WEBHOOK_SIGNING_KEY = ''
+const mg = mailgun({apiKey: '', domain: ''});
 const data = {
 	from: 'Excited User <me@samples.mailgun.org>',
 	to: 'paschalobba@gmail.com, paschal.obba@stu.cu.edu.ng',
